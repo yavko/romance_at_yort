5997b2a3101fe32d04abf8ae25b0309e7a8b998f
Ashlyn <ashlynyi1103@gmail.com>
2024-11-19 11:47:38 -0800

    scenes

--- MiscAssets.java
+++ MiscAssets.java
@@ -8,30 +8,27 @@ import java.util.HashMap;
 public class MiscAssets {
     
     public static HashMap<String, BufferedImage> backgrounds = new HashMap<>();
-    
+    private static void addBgImage(HashMap<String, BufferedImage> map, String fileName, String name) {
+        URL imgUrl = MiscAssets.class.getResource(fileName);
+        try {
+            BufferedImage img = ImageIO.read(imgUrl);
+            map.put(name, img);
+        } catch (IOException _ex) {
+            System.out.println("failed to load asset");   
+        }
+    }
     static {
-        URL bgImgUrl = MiscAssets.class.getResource("assets/opening_scene.png");
-        try {
-            BufferedImage img = ImageIO.read(bgImgUrl);
-            backgrounds.put("main", img);
-        } catch (IOException _ex) { System.out.println("failed to load asset"); }
+        addBgImage(backgrounds, "assets/opening_scene.png", "main");
         
-        URL baseBgUrl = MiscAssets.class.getResource("assets/bg.png");
-        try {
-            BufferedImage img = ImageIO.read(baseBgUrl);
-            backgrounds.put("base", img);
-        } catch (IOException _ex) { System.out.println("failed to load asset"); }
+        addBgImage(backgrounds, "assets/bg.png", "base");
         
-        URL bedroomImgUrl = MiscAssets.class.getResource("assets/bedroom.png");
-        try {
-            BufferedImage img = ImageIO.read(bedroomImgUrl);
-            backgrounds.put("bedroom", img);
-        } catch (IOException _ex) { System.out.println("failed to load asset"); }
+        addBgImage(backgrounds, "assets/bedroom.png", "bedroom");
+        
     }
     /**
      * Constructor for objects of class MiscAssets
      */
     public MiscAssets() {
-    
+        
     }
 }
