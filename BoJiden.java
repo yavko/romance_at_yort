2a097cf684aba68064426ce0b2552953be5dc311
Yavor Kolev <yavornkolev@gmail.com>
2024-11-21 19:08:10 -0800

    fix bojiden

--- BoJiden.java
+++ BoJiden.java
@@ -8,7 +8,7 @@ public class BoJiden extends engine.Character {
     public static final String name = "Bo Jiden";
     public static final String backgroundInfo = "to be written";
     public static int age = 45;
-    public BoJiden() {}
+    public BoJiden(int x, int y) { super(x,y); }
     public void displayEmotion(engine.Game game, engine.Emotion emote) {}
     public int getAge() { return age; }
     public void setAge(int newAge) { age = newAge; }
