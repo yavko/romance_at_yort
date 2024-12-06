911a9640c3ba9b1eec8e35dcd221299938c6318c
AshlynY <ashlynyi1103@gmail.com>
2024-12-05 22:35:48 -0800

    audio class and info link

---
+++ engine/MediaPlayer.java
@@ -0,0 +1,23 @@
+package engine;
+import java.io.File;
+//https://docs.oracle.com/javafx/2/api/javafx/scene/media/MediaPlayer.html#MediaPlayer(javafx.scene.media.Media)
+
+/**
+ * Write a description of class Audio here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public final class MediaPlayer
+extends java.lang.Object{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class Audio
+     */
+    public MediaPlayer(Media media)
+    {
+        
+    }
+}
